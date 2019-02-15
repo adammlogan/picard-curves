27f096cf78c9241acc7b611524d049c436de65c4
sachihashimoto <svh@bu.edu>
2019-02-15 10:21:49 -0500

    python parsing to find rank 1 curves

---
+++ findrankone.py
@@ -0,0 +1,14 @@
+#takes in a file of curves on which magma has run RankBounds and finds the rank one curves, prints to a file
+#you should manually remove the last comma by hand before running through magma
+
+def findRankOne(fin,fout):
+	out = open(fout,"w")
+	curves = open(fin,"r")
+	out.write('[')
+	for c in curves:
+		c=c.strip()
+		if int(c[-1:])==1 and int(c[-3:-2]) ==1:
+			c=c.replace(':',',')
+			out.write("["+c[:-3]+"]"+","+"\n")
+	out.write(']')
+findRankOne("testcurves.txt","testout.txt")
