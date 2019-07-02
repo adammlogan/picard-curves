5ca58699f295ae633d9e74947aeb0142cce5656d
Travis Morrison <tmo.3952@gmail.com>
2019-07-02 12:02:24 -0400

    Added reln_searchmod.m, which tests possible linear dependencies between a point defined over an extension and nontorsion rational points in the Jacobian

--- reln_searchmod.m
+++ reln_searchmod.m
@@ -1,78 +1,71 @@
 
-/* 
-    fin_places: a list of the non-infinite rational places
-    infty: the place above infinity = [0,1,0]
-    Q: point on our curve not defined over rationals
-    m: bound on the box size you want to search over
+
+
+
+/*
+This is from the example list of extra points. We find a relation
+between the point R not defined over the rationals and a
+point of infinite order on our curve. 
+
+_<x>:=PolynomialRing(Rationals());
+F := x^4+x^3-3*x^2-2*x;
+K<a> := NumberField(x^3+3*x^2+1);
+R := [a,a^2+2*a,1];
+reln_search(F,R,K,100,10);
+
+_<x>:=PolynomialRing(Rationals());
+F:= x^4 + 2*x^3 + 5*x^2 + 4*x + 1;
+K<a> := NumberField(x^3 - 1/16);
+R := [-1/2,a,1];
+reln_search(F,R,K,100,5);
+*/
+
+/* Seaches for relations over a box of size search_bnd 
+    in J(C) between a point R defined over 
+    the number field L, and points in C(Q) up to 
+    height. C is the Picard curve given by y^3-F=0. 
     
-    This creates the box [-m,m]*...*[-m,m]*[1,m] and 
-    checks whether  a_i*P_i +i*Q - w*infty is principal,
-    where a_i in [-m,m],
-    i in [1,m], 
-    and w = i + sum(a_i).
-    
-    It returns a list of all tuples of relations. 
     
 */
-function reln_search(fin_places,infty,Q,m)
-    I := [-m..m];
-    comps := [I : i in [1..#fin_places]];
-    Append(~comps,[1..m]);
+function reln_search(F,R,L,height,search_bnd)
+    P2 := ProjectiveSpace(Rationals(),2);
+    C := Curve(P2,Numerator(Evaluate(F,P2.1/P2.3)*P2.3^4-P2.3*P2.2^3));
+    I := [-search_bnd..search_bnd];
+    points := PointSearch(C,height);
+    fin_pts := Remove(points,Index(points,C![0,1,0]));
+    C := BaseChange(C,L);
+    R := Place(C!R);
+    fin_pts := [C![P[i] : i in [1..3]] : P in fin_pts];
+    infty := Place(C![0,1,0]);
+    fin_pts := [Place(P) : P in fin_pts];
+    non_tor := [];
+    for P in fin_pts do                 
+        if IsPrincipal(3*(P-infty)) then
+            continue;
+        else
+            Append(~non_tor,P);
+        end if;
+    end for;
+    comps := [I : i in [1..#non_tor]];
+    Append(~comps,[1..search_bnd]);
     box := CartesianProduct(comps);
     relns := [];
     for a in box do
         vector := [a[i] : i in [1..#a]];
         if Gcd(vector) eq 1 then
-            D := vector[#vector]*Q;
+            D := vector[#vector]*R;
             weight := vector[#vector];
             for i in [1..#vector-1] do
-                D := D + vector[i]*fin_places[i];
+                D := D + vector[i]*fin_pts[i];
                 weight := weight + vector[i];
             end for;
             D := D - weight*infty;
             if IsPrincipal(D) then 
-                Append(~relns,vector);
+                Append(~non_tor,R);
+                return vector, non_tor;
+                break;
             end if;
         end if;
     end for;
     return relns;
 end function;
-
-
-/*
-This is from the example list of extra points. We find a relation
-between the point Q not defined over the rationals and a
-point of infinite order on our curve. One remark: in this
-example, I removed the nontorsion points from my list of 
-rational points to speed up the search just for this example. 
-*/
-
-_<x>:=PolynomialRing(Rationals());
-f := x^4+x^3-3*x^2-2*x;
-K<a> := NumberField(x^3+3*x^2+1);
-P2 := ProjectiveSpace(Rationals(),2);
-C := Curve(P2,Numerator(Evaluate(f,P2.1/P2.3)*P2.3^4-P2.3*P2.2^3));
-points := PointSearch(C,100);
-C := BaseChange(C,K);
-Q := Place(C![a,a^2+2*a,1]);
-fin_pts := points[2..#points];
-fin_pts := [C![P[i] : i in [1..3]] : P in fin_pts];
-infty := Place(C![points[1][i]: i in [1..3]]);
-fin_pts := [Place(P) : P in fin_pts];
-non_tor := [fin_pts[2],fin_pts[4]];
-reln_search(non_tor,infty,Q,3);
-
-/*_<x>:=PolynomialRing(Rationals());
-f:= x^4 + 2*x^3 + 5*x^2 + 4*x + 1;
-K<a> := NumberField(x^3 - 1/16);
-P2 := ProjectiveSpace(Rationals(),2);
-C := Curve(P2,Numerator(Evaluate(f,P2.1/P2.3)*P2.3^4-P2.3*P2.2^3));
-points := PointSearch(C,100);
-C := BaseChange(C,K);
-Q := Place(C![-1/2,a,1]);
-fin_pts := points[2..#points];
-fin_pts := [C![P[i] : i in [1..3]] : P in fin_pts];
-infty := Place(C![points[1][i]: i in [1..3]]);
-fin_pts := [Place(P) : P in fin_pts];
-reln_search(fin_pts,infty,Q,5);
-*/
